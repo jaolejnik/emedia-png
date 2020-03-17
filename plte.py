7a71bdbe349266c1a15daa0036917fb8ffb4a022
Kuba <olejnik.jakub71@gmail.com>
2020-03-17 13:41:42 +0100

    Added a method to PLTE chunk class for plotting histograms.

--- plte.py
+++ plte.py
@@ -1,4 +1,6 @@
 from chunks import Chunk
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def init_palette():
@@ -15,9 +17,9 @@ class PLTE(Chunk):
         self.entries = length/3
         self.required = True if color_type == 3 else False
         self.palette = init_palette()
-        self.fill_pallete(data)
+        self.fill_palette(data)
 
-    def fill_pallete(self, data):
+    def fill_palette(self, data):
         for i in range(0, self.length, 3):
             self.palette["Red"][data[i]] += 1
             self.palette["Green"][data[i+1]] += 1
@@ -30,6 +32,22 @@ class PLTE(Chunk):
             for i in range(256):
                 if self.palette[color][i] == 0: continue
                 print("         {key}: {value}".format(key=i, value=self.palette[color][i]))
+
+    def plot_palette(self):
+        max_y = 0
+        for color in self.palette.keys():
+            if max(self.palette[color].values()) > max_y:
+                max_y = max(self.palette[color].values())
+        yticks = np.arange(0, max_y+1, 1)
+        fig, axs = plt.subplots(1,3)
+        plot_colors = ['r', 'g', 'b']
+        for i,color in enumerate(self.palette.keys()):
+            axs[i].set_yticks(yticks)
+            axs[i].set_ylim(0, max_y+1)
+            axs[i].set_xlim(0, 256)
+            axs[i].bar(self.palette[color].keys(), self.palette[color].values(), width=5, color=plot_colors[i])
+            axs[i].grid(axis='y', alpha=0.2)
+        plt.show()
 
     def print_info(self):
         self.basic_info()
--- test.py
+++ test.py
@@ -7,6 +7,8 @@ def parse_data(byte_string, chunk_type):
         return 0, None, None
 
     i = byte_string.find(chunk_type)
+    if i == -1:
+        raise Exception("Couldn't find {} chunk!".format(chunk_type))
     length = int(byte_string[i-4:i].replace(b"\x00", b"").hex(), 16)
     i += 4
     data = byte_string[i:i+length]
@@ -15,7 +17,7 @@ def parse_data(byte_string, chunk_type):
     return length, data, crc
 
 
-file = open("png_files/smiley.png", "rb") # open file
+file = open("png_files/pokemon.png", "rb") # open file
 byte_string = file.read() # read all of it's data bytes
 file.close() # close file
 
@@ -23,7 +25,9 @@ length, data, crc = parse_data(byte_string, b"IHDR")
 test_IHDR = IHDR(length, data, crc)
 test_IHDR.print_info()
 
-length, data, crc = parse_data(byte_string, b"PLTE")
-test_PLTE = PLTE(length, data, crc, test_IHDR.color_type)
-test_PLTE.print_info()
-test_PLTE.print_palette()
+if test_IHDR.color_type == 3:
+    length, data, crc = parse_data(byte_string, b"PLTE")
+    test_PLTE = PLTE(length, data, crc, test_IHDR.color_type)
+    test_PLTE.print_info()
+    test_PLTE.plot_palette()
+    # test_PLTE.print_palette()
