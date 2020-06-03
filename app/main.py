c57eacd90a6a7c37fa48d54d5081c202ac5d613c
jaolejnik <olejnik.jakub71@gmail.com>
2020-06-03 23:24:38 +0200

    added menu

--- app/main.py
+++ app/main.py
@@ -1,43 +1,5 @@
 from menu import Menu
 from data import FilePNG
-from own_rsa import RSA
 
-file = FilePNG("../png_files/ship.png")
-data = file.chunks['IDAT'].reconstructed_data
-# file.chunks['IDAT'].display_data("Primary")
-key_size = 32
-
-file = FilePNG("../png_files/dice.png")
-data = file.chunks['IDAT'].reconstructed_data
-file.chunks['IDAT'].display_data("ORYGINAŁ")
-
-key_size = 1024
-
-
-step = key_size // 8 - 1
-data = [data[i:i+step] for i in range(0, len(data), step)]
-data = [bytearray(slice) for slice in data]
-data_sizes = [int.from_bytes(slice, 'big') for slice in data]
-print(max(data_sizes))
-
-
-rsa = RSA(max(data_sizes), key_size)
-
-# print(rsa.check_if_encryption_correct(file.chunks['IDAT'].reconstructed_data))
-# print(type(file.chunks['IDAT'].compressed_data))
-# file.chunks['IDAT'].reconstructed_data = rsa.encryption(file.chunks['IDAT'].compressed_data)
-# file.chunks['IDAT'].display_data("Primary")
-# file.chunks['IDAT'].reconstructed_data = rsa.decryption(file.chunks['IDAT'].decompressed_data)
-# file.chunks['IDAT'].display_data("Primary")
-
-file.chunks['IDAT'].reconstructed_data = rsa.encryption_cbc(file.chunks['IDAT'].reconstructed_data)
-file.chunks['IDAT'].display_data("ZASZYFROWANE", data=[float(x) for x in file.chunks['IDAT'].reconstructed_data])
-file.chunks['IDAT'].reconstructed_data = rsa.decryption_cbc(file.chunks['IDAT'].reconstructed_data)
-file.chunks['IDAT'].display_data("ROZSZYFROWANE")
-
-# file.print_to_file()
-
-# file.chunks['IDAT'].reconstructed_data = rsa.encrypt_with_ready_solution(file.chunks['IDAT'].reconstructed_data)
-# file.chunks['IDAT'].display_data("Primary")
-# file.chunks['IDAT'].reconstructed_data = rsa.decrypt_with_ready_solution(file.chunks['IDAT'].reconstructed_data)
-# file.chunks['IDAT'].display_data("Primary")
+menu = Menu()
+menu.start()
--- app/menu.py
+++ app/menu.py
@@ -2,9 +2,8 @@ import re
 from glob import glob
 from os import system
 from matplotlib import pyplot as plt
-
 from data import FilePNG
-
+from rsa import RSA
 
 class Menu:
     '''
@@ -14,6 +13,7 @@ class Menu:
         self.choice = None
         self.active_menu = self.main_menu
         self.active_options = self.main_options
+        self.rsa = None
 
     def set_file_list(self, dir_path):
         '''
@@ -60,10 +60,25 @@ class Menu:
         print(" [3] - Go to chunks menu (for more details).")
         print(" [4] - Save as a new file with only critical chunks.")
         print(" [5] - Perform FFT.")
+        print(" [6] - Cryptography")
         print()
         print(" [B] - Go back.")
         print(" [Q] - Quit\n")
 
+    @staticmethod
+    def crypt_options():
+        '''
+        Displays possible options for the crypt menu.
+        '''
+        print("WHAT DO YOU WANT TO DO NEXT?\n")
+        print(" [1] - Encrypt (selfmade, EBC)")
+        print(" [2] - Decrypt (selfmade, EBC)")
+        print()
+        print(" [3] - Encrypt (selfmade, CBC)")
+        print(" [4] - Decrypt (selfmade, CBC)")
+        print()
+        print(" [B] - Go back.")
+        print(" [Q] - Quit\n")
 
     def chunks_options(self):
         '''
@@ -140,12 +155,17 @@ class Menu:
             self.active_menu = self.chunks_menu
             self.active_options = self.chunks_options
 
+        def switch_crypt_menu():
+            self.active_menu = self.crypt_menu
+            self.active_options = self.crypt_options
+
         switch = {
         '1': self.original_file.print_info,
         '2': self.original_file.print_chunks,
         '3': switch_chunks_menu,
         '4': self.original_file.print_to_file,
         '5': self.original_file.perform_fft,
+        '6': switch_crypt_menu,
         'b': go_back,
         'q': exit,
         }
@@ -182,6 +202,69 @@ class Menu:
             switch['p'] = palette_application
         if "tRNS" in self.original_file.chunks.keys():
             switch['t'] = transparency_application
+        choice = input(">> ").lower()
+        system("clear")
+        switch.get(choice, Menu.invalid_option)()
+
+    def crypt_menu(self):
+        encrypted_data = None
+        decrypted_data = None
+
+        def encrypt():
+            global encrypted_data
+            system("clear")
+            print("Specify key size:")
+            key_size = int(input(">> ").lower())
+            data = self.original_file.chunks['IDAT'].reconstructed_data
+            step = key_size // 8 - 1
+            data = [data[i:i+step] for i in range(0, len(data), step)]
+            data = [bytearray(slice) for slice in data]
+            data_sizes = [int.from_bytes(slice, 'big') for slice in data]
+            self.rsa = RSA(max(data_sizes), key_size)
+
+            self.original_file.chunks['IDAT'].display_data("ORIGINAL")
+            encrypted_data = self.rsa.encryption(self.original_file.chunks['IDAT'].reconstructed_data)
+            self.original_file.chunks['IDAT'].display_data("ENCRYPTED", data=encrypted_data)
+
+        def decrypt():
+            global decrypted_data, encrypted_data
+            decrypted_data = self.rsa.decryption(encrypted_data)
+            self.original_file.chunks['IDAT'].display_data("DECRYPTED", data=decrypted_data)
+
+        def encrypt_cbc():
+            global encrypted_data
+            system("clear")
+            print("Specify key size:")
+            key_size = int(input(">> ").lower())
+            data = self.original_file.chunks['IDAT'].reconstructed_data
+            step = key_size // 8 - 1
+            data = [data[i:i+step] for i in range(0, len(data), step)]
+            data = [bytearray(slice) for slice in data]
+            data_sizes = [int.from_bytes(slice, 'big') for slice in data]
+            self.rsa = RSA(max(data_sizes), key_size)
+
+            self.original_file.chunks['IDAT'].display_data("ORIGINAL")
+            encrypted_data = self.rsa.encryption_cbc(self.original_file.chunks['IDAT'].reconstructed_data)
+            self.original_file.chunks['IDAT'].display_data("ENCRYPTED CBC", data=[float(x) for x in encrypted_data])
+
+        def decrypt_cbc():
+            global decrypted_data, encrypted_data
+            decrypted_data = self.rsa.decryption_cbc(encrypted_data)
+            self.original_file.chunks['IDAT'].display_data("DECRYPTED CBC", data=decrypted_data)
+
+        def go_back():
+            self.active_menu = self.file_menu
+            self.active_options = Menu.file_options
+
+
+        switch = {
+        '1': encrypt,
+        '2': decrypt,
+        '3': encrypt_cbc,
+        '4': decrypt_cbc,
+        'b': go_back,
+        'q': exit,
+        }
         choice = input(">> ").lower()
         system("clear")
         switch.get(choice, Menu.invalid_option)()
